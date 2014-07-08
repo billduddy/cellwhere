3c32ea7a4122d92548308937a5f3b334c6765489
unknown <zhu.lu@hotmail.com>
2014-07-08 16:48:05 +0200

    Signed-off-by: unknown <zhu.lu@hotmail.com>

--- php/process.php
+++ php/process.php
@@ -340,7 +340,6 @@ $start = microtime(true);
 switch ($ID_type) {                 // Depending on selection on dropdown menu
     case "GeneSymbol":
         foreach ( $ID_array as $key => $value ) {
-	    echo $value."/";
             if (empty($value)) {continue;}      // Ignore empty values
             $TheseACCs = '';                    // Initialize array to store ACCs retrieved for a single query ID
             if ($HumMouseFlag == "HumMouseOverlap") {
@@ -355,7 +354,7 @@ switch ($ID_type) {                 // Depending on selection on dropdown menu
             array_shift($TheseACCs);
             foreach ( $TheseACCs as $key2 => $value2 ) {    // Make table with separate row for the query ID (1st column)
                 $TheseACCs[$key2] = "$value\t$value2";      // and each retrieved ACC (2nd column)
-		echo $value2;
+		echo "$value\t$value2";
             }
             $ACCfromUniprot .= implode("\n", $TheseACCs);   // Successively combine tables of all Query IDs and their retrieved ACCs
             $ACCfromUniprot .= "\n";
