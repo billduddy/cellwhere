e7f0cf069a46568d4d394a1d5846f0e98546804d
unknown <zhu.lu@hotmail.com>
2014-07-09 13:41:13 +0200

    Signed-off-by: unknown <zhu.lu@hotmail.com>

--- php/process.php
+++ php/process.php
@@ -404,7 +404,7 @@ switch ($ID_type) {                 // Depending on selection on dropdown menu
 	}
         $url = 'http://www.uniprot.org/mapping/';           // Next ~10 lines POST to Uniprot mapping service using CURL 
         $TheseIDs = implode(",", $ID_array);
-	echo $TheseIDs;
+	echo "   TheseIDs".$TheseIDs;
         $myvars = 'from=' . 'ENSEMBL_ID' . '&to=' . 'ACC' . '&format=' . 'tab' . '&query=' . $TheseIDs;
         $ch = curl_init( $url );
         curl_setopt( $ch, CURLOPT_POST, 1);
