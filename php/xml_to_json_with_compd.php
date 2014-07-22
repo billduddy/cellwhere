cc5580fe42646c159eaec6062de21670496f29e5
unknown <zhu.lu@hotmail.com>
2014-07-22 11:22:40 +0200

    Signed-off-by: unknown <zhu.lu@hotmail.com>

--- php/xml_to_json_with_compd.php
+++ php/xml_to_json_with_compd.php
@@ -244,8 +244,7 @@
 	$str=file_get_contents($new_xml_file."_web.html"); 
 	$str=str_replace('{"nodes":['."\n,",'{"nodes":['."",$str);
 	$str=str_replace('"style":['."\n,",'"style":['."",$str);
-	$str=str_replace('
-",','",',$str);				// be careful!!  some return causes display errors
+	$str=str_replace("\n".'",','",',$str);				// be careful!!  some return causes display errors
 	file_put_contents($new_xml_file."_web.html", $str);
     }
 ?>
